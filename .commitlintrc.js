73ff34ee9406c793e3b9863c27d3c0b03797e552
7mus1c <7mus1c@163.com>
2024-07-10 18:31:43 +0800

    feat: 初始化工程化配置

---
+++ .commitlintrc.js
@@ -0,0 +1,11 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Autor: Li Cheng
+ * @Date: 2024-07-10 16:58:36
+ * @LastEditors: Li Cheng
+ * @LastEditTime: 2024-07-10 16:59:12
+ */
+module.exports = {
+	extends: ['@commitlint/config-conventional']
+};
---
+++ packages/1.ts
@@ -0,0 +1,11 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Autor: Li Cheng
+ * @Date: 2024-07-10 17:05:43
+ * @LastEditors: Li Cheng
+ * @LastEditTime: 2024-07-10 17:50:25
+ */
+
+const a = 11;
+console.log(a);
